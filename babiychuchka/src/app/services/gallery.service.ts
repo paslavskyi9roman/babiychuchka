0a2c89a31484ace12013dca928afb2e0321ab823
Roman.Paslavskyi <Roman.Paslavskyi@medhost.com>
2022-08-03 00:15:50 +0300

    fetching data from a server

--- babiychuchka/src/app/services/gallery.service.ts
+++ babiychuchka/src/app/services/gallery.service.ts
@@ -1,24 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, take } from 'rxjs';
+import { map, Observable, Subject, take } from 'rxjs';
 
-import { paintingsMock } from '../shared/mock-data/paintings.mock';
 import { Painting } from '../shared/models/painting.model';
+
 @Injectable({
   providedIn: 'root',
 })
 export class GalleryService {
-  public paintingsStorage$ = new BehaviorSubject<Painting[]>(paintingsMock);
+  private paintingsStorage$ = new Subject<Painting[]>();
   public paintings$ = this.paintingsStorage$.asObservable();
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  public getPaintings() {
+    this.http
+      .get<{ message: string; paintings: Painting[] }>('http://localhost:3000/api/paintings')
+      .subscribe((data) => {
+        this.paintingsStorage$.next([...data.paintings]);
+      });
+  }
 
   public getPaintinById(id: string): Observable<Painting[]> {
     return this.paintings$.pipe(map((painting) => painting.filter((el) => el.id === id)));
   }
-
-  // public addPainting(painting: Painting) {
-  //   paintingsMock.push(painting);
-  // }
 
   public addPainting(painting: Painting) {
     this.paintings$.pipe(take(1)).subscribe((paintings) => {
