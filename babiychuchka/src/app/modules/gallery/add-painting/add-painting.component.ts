ab656c63653854ac3f36f75a2abbe80aeca1b759
Roman.Paslavskyi <Roman.Paslavskyi@medhost.com>
2022-08-02 12:17:30 +0300

    creating add-painting form

--- babiychuchka/src/app/modules/gallery/add-painting/add-painting.component.ts
+++ babiychuchka/src/app/modules/gallery/add-painting/add-painting.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-painting',
   templateUrl: './add-painting.component.html',
-  styleUrls: ['./add-painting.component.scss']
+  styleUrls: ['./add-painting.component.scss'],
 })
 export class AddPaintingComponent implements OnInit {
+  paintingForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.paintingForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      url: ['', Validators.required],
+    });
   }
 
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid);
+    console.log(form.value.title);
+    console.log(form.value.description);
+    console.log(form.value.url);
+  }
 }
--- babiychuchka/src/app/modules/gallery/gallery.module.ts
+++ babiychuchka/src/app/modules/gallery/gallery.module.ts
@@ -6,9 +6,10 @@ import { GalleryComponent } from './gallery.component';
 import { PaintingComponent } from './painting/painting.component';
 import { AddPaintingComponent } from './add-painting/add-painting.component';
 import { EditPaintingComponent } from './edit-painting/edit-painting.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [GalleryComponent, PaintingComponent, AddPaintingComponent, EditPaintingComponent],
-  imports: [CommonModule, GalleryRoutingModule],
+  imports: [CommonModule, GalleryRoutingModule, ReactiveFormsModule],
 })
 export class GalleryModule {}
