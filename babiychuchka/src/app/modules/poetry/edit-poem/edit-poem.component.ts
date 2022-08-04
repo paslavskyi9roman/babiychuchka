8d1b1a4e20941e58e08a1ebd8773c6a119e02088
Roman.Paslavskyi <Roman.Paslavskyi@medhost.com>
2022-08-04 14:59:06 +0300

    added new components for poetry

---
+++ babiychuchka/src/app/modules/poetry/edit-poem/edit-poem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-poem',
+  templateUrl: './edit-poem.component.html',
+  styleUrls: ['./edit-poem.component.scss']
+})
+export class EditPoemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
