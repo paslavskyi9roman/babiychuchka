c460b172a42b4547f38559b46e446fe41f2badae
Roman.Paslavskyi <Roman.Paslavskyi@medhost.com>
2022-08-27 20:09:39 +0300

    optimizing the service

--- babiychuchka/src/app/services/gallery.service.ts
+++ babiychuchka/src/app/services/gallery.service.ts
@@ -10,15 +10,14 @@ import { Painting } from '../shared/models/painting.model';
 export class GalleryService {
   private paintingsStorage$ = new Subject<Painting[]>();
   public paintings$ = this.paintingsStorage$.asObservable();
+  private readonly url = `http://localhost:3000/api`;
 
   constructor(private http: HttpClient) {}
 
   public getPaintings() {
-    this.http
-      .get<{ message: string; paintings: Painting[] }>('http://localhost:3000/api/paintings')
-      .subscribe((data) => {
-        this.paintingsStorage$.next([...data.paintings]);
-      });
+    this.http.get<{ message: string; paintings: Painting[] }>(`${this.url}/paintings`).subscribe((data) => {
+      this.paintingsStorage$.next([...data.paintings]);
+    });
   }
 
   public getPaintinById(id: string): Observable<Painting[]> {
@@ -26,7 +25,7 @@ export class GalleryService {
   }
 
   public addPainting(painting: Painting): void {
-    this.http.post<{ message: string }>('http://localhost:3000/api/paintings', painting).subscribe((data) => {
+    this.http.post<{ message: string }>(`${this.url}/paintings`, painting).subscribe((data) => {
       this.paintings$.pipe(take(1)).subscribe((data) => {
         this.paintingsStorage$.next([...data, painting]);
       });
@@ -35,7 +34,7 @@ export class GalleryService {
   }
 
   public deletePaintings(paintingId: string): void {
-    this.http.delete('http://localhost:3000/api/paintings/' + paintingId).subscribe(() => {
+    this.http.delete(`${this.url}/paintings` + paintingId).subscribe(() => {
       this.getPaintings();
     });
   }
