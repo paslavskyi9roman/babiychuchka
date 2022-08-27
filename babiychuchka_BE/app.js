9a98828de41b1cc04c33b35197fcb41efa9c56b1
Roman.Paslavskyi <Roman.Paslavskyi@medhost.com>
2022-08-27 21:29:16 +0300

    adding mock poems

--- babiychuchka_BE/app.js
+++ babiychuchka_BE/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const Painting = require('./models/Painting');
 
 mongoose
-  .connect('mongodb+srv://@cluster0.hc5onky.mongodb.net/babiychuchka?retryWrites=true&w=majority')
+  .connect('')
   .then(() => {
     console.log('Connected to database!');
   })
@@ -57,26 +57,26 @@ app.get('/api/poetry', (req, res, next) => {
   const poetry = [
     {
       id: '1',
-      title: 'ghowfk;ALFak',
-      text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+      title: '«Осінній вітре, що могучим стоном…»',
+      text: 'Осінній вітре, що могучим стоном Над лісом стогнеш, мов над сином мати, Що хмари люто гониш небосклоном, Мов хочеш зиму, сон і смерть прогнати; Що у щілинах диким виєш тоном І рвеш солому із сільської хати, Зів’яле листя гоном-перегоном По полю котиш, – вітре мій крилатий! Я довго пильно слухав стону твого І знаю, чом так стогнеш ти і плачеш: Тобі жаль сонця, цвіту, дня літнього! О вітре-брате! Як мене побачиш Старим, зів’ялим, чи й по мні заплачеш, Чи гнівно слід буття завієш мого?..',
       date: '01/01/2018',
     },
     {
       id: '2',
-      title: 'snagjlsdgknsgnklas',
-      text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+      title: 'Vivere memento!',
+      text: 'Весно, що за чудо ти Твориш в моїй груди? Чи твій поклик з мертвоти Й серце к жизні будить? Вчора тлів, мов Лазар, я В горя домовині – Що ж се за нова зоря Мені блисла нині? Дивний голос мя кудись Кличе – тут-то, ген-то: «Встань, прокинься, пробудись! Vivere memento!» Вітре теплий, брате мій, Чи твоя се мова? Чи на гірці світляній Так шумить діброва? Травко, чи се, може, та Втішно так шептала, Що з-під криги мертвоти Знов на світло встала? Чи се, може, шемріт твій, Річко, срібна ленто, Змив мій смуток і застій? Vivere memento! Всюди чую любий глас, Клик життя могучий… Весно, вітре, люблю вас, Гори, ріки, тучі! Люди, люди! Я ваш брат, Я для вас рад жити, Серця свого кров’ю рад Ваше горе змити. А що кров не зможе змить, Спалимо огнем то! Лиш боротись значить жить… Vivere memento!',
       date: '01/01/2018',
     },
     {
       id: '3',
-      title: 'hgshgsoierouergh',
-      text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+      title: '«Ночі безмірнії, ночі безсоннії…»',
+      text: 'Ночі безмірнії, ночі безсоннії, Горе моє! Мозок наляжуть думки невгомоннії, В серці грижа, мов павук той, полоннії Сіті снує. Виром невпинним бажання сердечнії Рвуться, летять – Вічно невтишені і безконечнії… Мов на свої мене крила безпечнії Схопить хотять. Де ви так рветесь, куди ви літаєте, Думи-орли? В гості до зірки ви, чень, не бажаєте? К земним зіркам же ви й стежки не знаєте Тут по земли. О моя ясна, блискуча зірничко, Де ти живеш? Чи за життя ще я вздрю твоє личко? Чи аж по смерті на гріб мій, горличко, Плакать прийдеш?',
       date: '01/01/2018',
     },
     {
       id: '4',
-      title: 'ryqoirqwroyqro',
-      text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+      title: 'Пісня геніїв ночі',
+      text: 'По боях земного життя Чи в повні свіжих сил; В утомі з довгого пуття Чи в першім маху крил; Чи вниз літа склонили скрань, Чи в серці рай весни, – Сюди, вандрівче, ти пристань! Засни! Засни! Засни! Що земний шлях, що земний бій, Ненависть і любов? Тут ніч, тиша і супокій Без снів і без оков. Тут болю, ані втіх нема, Морозу, ні весни, Тут забуття, спокій і тьма – Засни! Засни! Засни! Припадь істотою цілов Природі до грудий, Туди верни, відкіль прийшов, Напій безсмертя пий! Почате в бою тіло те Знов в вир бездонний пхни, Що вічно смерть з життям плете, – Засни! Засни! Засни! А дух? Се ж іскорка лишень, Се огник, нервів рух! Розпадесь мозок, то й огень Погасне, згине дух. Воскресних не лякайсь казок, Хай для дітей вони! Остатній біль – побідний крок! Засни! Засни! Засни!',
       date: '01/01/2018',
     },
   ];
